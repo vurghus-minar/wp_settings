32359bbf831c32336a1445570cf2a4d3636643be
vurghus-minar <vurghus.minar@outlook.com>
2019-07-01 17:30:59 +0400

    integrated wp scripts

--- src/App.js
+++ src/App.js
@@ -1,74 +1,90 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import './style.scss';
 
-const __ = window.wp.i18n.__;
+const { Component } = wp.element;
+const { __ } = wp.i18n;
 
-export default class App extends React.Component{
-
-  constructor (props){
+export default class App extends Component {
+  constructor(props) {
     super(props);
-    this.axios = axios.create({
-      baseURL: window.example_rest_object.rest_url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-WP-Nonce': window.example_rest_object.nonce
-      }
-    });
-  }  
-  
-  state = {
-    options: {
-      bla: '' 
-    }
+    this.state = {
+      options: {
+        bla: '',
+      },
+    };
   }
 
-  componentDidMount(){
-    this.axios.get('/')
-      .then(res=>{
-        const options = res.data.value;
-        this.setState({options});
+  componentDidMount() {
+    this.ajax('GET')
+      .then(res => {
+        if (res.success) {
+          const options = res.value;
+          this.setState({ options });
+        }
       })
-      .catch(error=>{
+      .catch(error => {
         console.log(error);
       });
   }
 
-  handleChange = event => {
-    this.setState({ bla: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const options = {
-      bla: this.state.options.bla
+  ajax(method) {
+    const config = {
+      method,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-WP-Nonce': window.example_rest_object.nonce,
+      },
     };
-
-    this.axios.post('/', { options })
-      .then(res => {
-        
+    return fetch(window.example_rest_object.rest_url, config)
+      .then(res => res.json().then(data => (res.ok ? data : Promise.reject(data))))
+      .catch(error => {
+        Promise.reject(error);
       });
   }
 
-  render(){
-    return(
+  handleChange(event) {
+    this.setState({ bla: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const options = {
+      bla: this.state.options.bla,
+    };
+
+    // this.ajax('POST');
+  }
+
+  render() {
+    return (
       <section className="settings-form">
         <form onSubmit={this.handleSubmit}>
           <table className="form-table">
             <tbody>
               <tr>
                 <th scope="row">
-                <label>{__('Bla:', 'am-boilerplate-plugin')}</label>
+                  <label>{__('Bla:', 'am-boilerplate-plugin')}</label>
                 </th>
-                <td><input className="regular-text" type="text" name="bla" value={ this.state.options.bla } onChange={this.handleChange} /></td>              
-              </tr>              
+                <td>
+                  <input
+                    className="regular-text"
+                    type="text"
+                    name="bla"
+                    value={this.state.options.bla}
+                    onChange={this.handleChange}
+                  />
+                </td>
+              </tr>
             </tbody>
           </table>
-          <p className="submit"><button className="button button-primary" type="submit">Save</button></p>
+          <p className="submit">
+            <button className="button button-primary" type="submit">
+              Save
+            </button>
+          </p>
         </form>
       </section>
-    )
+    );
   }
 }
